9c305eca61dd77e661dfc8635107bd8e21306e73
ghost2004 <liutg@hotmail.com>
2016-03-11 23:05:42 -0800

    Leetcode 115. Distinct Subsequences

---
+++ src/dynamicProgramming/DistinctSubsequences.java
@@ -0,0 +1,37 @@
+package dynamicProgramming;
+
+/*
+ * Leetcode 115. Distinct Subsequences
+ * 
+ * Given a string S and a string T, count the number of distinct subsequences of T in S.
+
+A subsequence of a string is a new string which is formed from the original string by 
+deleting some (can be none) of the characters without disturbing the relative positions
+ of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+Here is an example:
+S = "rabbbit", T = "rabbit"
+
+Return 3.
+ */
+public class DistinctSubsequences {
+    public int numDistinct(String s, String t) {
+        int tLen = t.length();
+        int sLen = s.length();
+        int dp[][] = new int [sLen+1][tLen+1];
+        
+        for (int i = 0; i <= sLen; i++)
+            dp[i][0] = 1;
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= tLen; j++) {
+                dp[i][j] = dp[i-1][j];
+                if (s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] += dp[i-1][j-1];
+                }
+            }
+        }
+        
+        return dp[sLen][tLen];
+    }
+
+}
