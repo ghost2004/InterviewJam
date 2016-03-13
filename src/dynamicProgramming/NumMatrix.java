0e2898fdb2aa7d1a57f332171cf0447c066650c7
ghost2004 <liutg@hotmail.com>
2016-03-13 00:17:46 -0800

    304. Range Sum Query 2D - Immutable

--- src/dynamicProgramming/NumMatrix.java
+++ src/dynamicProgramming/NumMatrix.java
@@ -1,5 +1,6 @@
 package dynamicProgramming;
 /*
+ * Leetcode 304. Range Sum Query 2D - Immutable
  * Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its 
  * upper left corner (row1, col1) and lower right corner (row2, col2).
 
@@ -24,5 +25,29 @@ There are many calls to sumRegion function.
 You may assume that row1 <= row2 and col1 <= col2.
  */
 public class NumMatrix {
+
+    int dp[][];
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            dp = new int[1][1];
+            dp[0][0] = 0;
+            return;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        dp = new int[m+1][n+1];
+
+        for (int i = 1; i <= m ; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i-1][j-1];
+            }
+            
+        }
+        
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return (dp[row2+1][col2+1]+dp[row1][col1] - dp[row1][col2+1]-dp[row2+1][col1]);
+    }
 
 }
