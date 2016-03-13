a823d8d5d6bff34016f70305db896aa181900b5e
ghost2004 <liutg@hotmail.com>
2016-03-12 22:35:27 -0800

    tesst

--- src/dynamicProgramming/NumArray.java
+++ src/dynamicProgramming/NumArray.java
@@ -2,7 +2,7 @@ package dynamicProgramming;
 /*
  * Leetcode 303. Range Sum Query - Immutable
  * Given an integer array nums, find the sum of the elements 
- * between indices i and j (i ¡Ü j), inclusive.
+ * between indices i and j (i <= j), inclusive.
 
 Example:
 Given nums = [-2, 0, 3, -5, 2, -1]
