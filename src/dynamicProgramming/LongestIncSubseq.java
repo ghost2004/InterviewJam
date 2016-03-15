31bc53ed196c54e1eb697d46ba10f8880bcc4d49
ghost2004 <liutg@hotmail.com>
2016-03-14 23:59:44 -0700

    Leetcode 300. Longest Increasing Subsequence

---
+++ src/dynamicProgramming/LongestIncSubseq.java
@@ -0,0 +1,39 @@
+package dynamicProgramming;
+/*
+ * Leetcode 300. Longest Increasing Subsequence
+ *  Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+For example,
+Given [10, 9, 2, 5, 3, 7, 101, 18],
+The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. Note that 
+there may be more than one LIS combination, it is only necessary for you to return the length.
+
+Your algorithm should run in O(n2) complexity.
+
+Follow up: Could you improve it to O(n log n) time complexity?
+ */
+public class LongestIncSubseq {
+    /* solution of O(n2) */
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int len = nums.length;
+        // dp[i] means longest increasing subsequence at i
+        int dp[] = new int[len];
+        int max = 1;
+        for (int i = 0; i < len; i++) {
+            // at least it is 1
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i])
+                    // less than the end of sub-array
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+            }
+            // update max value
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+    
+    /* solution of O(n2) */
+}
