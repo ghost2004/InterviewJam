7d5a1cc451e046412f91843be3b597ec8d4f7687
ghost2004 <liutg@hotmail.com>
2016-03-20 22:26:15 -0700

    Leetcode 53 53. Maximum Subarray

---
+++ src/dynamicProgramming/MaxSubarray.java
@@ -0,0 +1,24 @@
+package dynamicProgramming;
+/*
+ * Leetcode 53 53. Maximum Subarray
+ * Find the contiguous subarray within an array (containing at least one number)
+ *  which has the largest sum.
+ */
+public class MaxSubarray {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) 
+            return 0;
+        int max = nums[0];
+        int curMax = max;
+        for (int i = 1; i < nums.length; i++) {
+            
+            if (curMax < 0)
+                curMax = nums[i];
+            else 
+                curMax += nums[i];
+            max = Math.max(curMax, max);
+        }
+        return max;
+    }
+
+}
