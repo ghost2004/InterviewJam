5abe5b24993d06b1e077c683bbfefebcaecef993
ghost2004 <liutg@hotmail.com>
2016-03-20 23:39:46 -0700

    Leetcode 279. Perfect Squares draft

---
+++ src/dynamicProgramming/PerfectSquares.java
@@ -0,0 +1,17 @@
+package dynamicProgramming;
+/*
+ * Leetcode 279. Perfect Squares
+
+ * Given a positive integer n, find the least number of perfect square numbers 
+ * (for example, 1, 4, 9, 16, ...) which sum to n.
+
+For example, given n = 12, return 3 because 12 = 4 + 4 + 4;
+ given n = 13, return 2 because 13 = 4 + 9.
+ */
+public class PerfectSquares {
+    public int numSquares(int n) {
+        int dp[] = new int[n+1];
+
+        return dp[n];
+    }
+}
