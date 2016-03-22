c0c8ce5834d7848e748b9ca8e101765e4832a78f
ghost2004 <liutg@hotmail.com>
2016-03-21 22:29:25 -0700

    Leetcode 279. Perfect Squares

--- src/dynamicProgramming/PerfectSquares.java
+++ src/dynamicProgramming/PerfectSquares.java
@@ -1,4 +1,7 @@
 package dynamicProgramming;
+
+import java.util.Arrays;
+
 /*
  * Leetcode 279. Perfect Squares
 
@@ -11,6 +14,13 @@ For example, given n = 12, return 3 because 12 = 4 + 4 + 4;
 public class PerfectSquares {
     public int numSquares(int n) {
         int dp[] = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; i+j*j <= n;j++) {
+                dp[i+j*j] = Math.min(dp[i+j*j], dp[i]+1);
+            }
+        }
 
         return dp[n];
     }
