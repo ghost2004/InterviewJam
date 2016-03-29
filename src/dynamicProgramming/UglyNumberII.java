9227d625bc0616e806d07c5de51fbcac5bdee4d2
ghost2004 <liutg@hotmail.com>
2016-03-28 22:48:13 -0700

    Leetcode 264. Ugly Number II

--- src/dynamicProgramming/UglyNumberII.java
+++ src/dynamicProgramming/UglyNumberII.java
@@ -9,5 +9,30 @@ Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
 Note that 1 is typically treated as an ugly number.
  */
 public class UglyNumberII {
-
+    public int nthUglyNumber(int n) {
+        int ugly[] = new int[n];
+        ugly[0] = 1;
+        // index for 2 , 3, 5
+        int idx[] = {0, 0, 0};
+        int base[] = {2, 3, 5};
+        for (int i = 1; i < n; i++) {
+            int pt = 0;
+            for (int j = 1; j < 3;j++)  
+                if (base[j] * ugly[idx[j]] < base[pt] * ugly[idx[pt]])
+                    pt = j;
+            
+            ugly[i] = base[pt] * ugly[idx[pt]];
+            // skip the duplicate number
+            if (ugly[i] == ugly[i-1])
+                i--;
+            idx[pt]++;
+        }
+        return ugly[n-1];
+    }
+    
+    public static void main(String args[]) {
+        UglyNumberII u = new UglyNumberII();
+        //for (int i = 1; i <= 10; i++)
+        System.out.println(u.nthUglyNumber(7));
+    }
 }
