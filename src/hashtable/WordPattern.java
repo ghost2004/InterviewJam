c7a62b746a2b15cdfd1c0fe4a60d703a4c940dbd
ghost2004 <liutg@hotmail.com>
2016-04-10 22:42:13 -0700

    Leetcode 290. Word Pattern

---
+++ src/hashtable/WordPattern.java
@@ -0,0 +1,49 @@
+package hashtable;
+/*
+ * Leetcode 290. Word Pattern
+ * Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter 
+in pattern and a non-empty word in str.
+
+Examples:
+pattern = "abba", str = "dog cat cat dog" should return true.
+pattern = "abba", str = "dog cat cat fish" should return false.
+pattern = "aaaa", str = "dog cat cat dog" should return false.
+pattern = "abba", str = "dog dog dog dog" should return false.
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase
+ letters separated by a single space.
+ */
+import java.util.*;
+public class WordPattern {
+    public boolean wordPattern(String pattern, String str) {
+        if (pattern == null || str == null)
+            return false;
+        HashMap<Character,String> keyMap = new HashMap<Character,String>();
+        HashMap<String, Character> valMap = new HashMap<String, Character>();
+        int idx = 0;
+        String s[] = str.split(" ");
+        if (pattern.length() != s.length)
+            return false;
+        for (idx = 0; idx < s.length; idx++) {
+            Character c = pattern.charAt(idx);
+            String val = keyMap.get(c);
+            if (val == null) {
+                keyMap.put(c, s[idx]);
+            } else {
+                if (!val.equals(s[idx]))
+                    return false;
+            }
+            c = valMap.get(s[idx]);
+            if (c == null) {
+                valMap.put(s[idx], pattern.charAt(idx));
+            } else {
+                if (c != pattern.charAt(idx))
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
