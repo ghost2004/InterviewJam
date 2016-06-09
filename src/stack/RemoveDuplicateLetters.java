02ed086ca660075152a42e42a0848b16496f366f
ghost2004 <liutg@hotmail.com>
2016-06-09 00:10:04 -0700

    316. Remove Duplicate Letters draft

---
+++ src/stack/RemoveDuplicateLetters.java
@@ -0,0 +1,21 @@
+package stack;
+/*
+ * Given a string which contains only lowercase letters, remove duplicate letters so that 
+ * every letter appear once and only once. You must make sure your result is the smallest
+ *  in lexicographical order among all possible results.
+
+Example:
+Given "bcabc"
+Return "abc"
+
+Given "cbacdcbc"
+Return "acdb"
+ */
+public class RemoveDuplicateLetters {
+    public String removeDuplicateLetters(String s) {
+        if (s == null || s.length() < 2)
+            return s;
+        
+        return s;
+    }
+}
