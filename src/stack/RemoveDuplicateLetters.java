caaa2a3c10b85107184dd542bb6d5871de7689de
ghost2004 <liutg@hotmail.com>
2016-06-12 22:19:00 -0700

    Leetcode 316. Remove Duplicate Letters

--- src/stack/RemoveDuplicateLetters.java
+++ src/stack/RemoveDuplicateLetters.java
@@ -11,6 +11,7 @@ Return "abc"
 Given "cbacdcbc"
 Return "acdb"
  */
+import java.util.*;
 public class RemoveDuplicateLetters {
     public String removeDuplicateLetters(String s) {
         if (s == null || s.length() < 2)
@@ -18,8 +19,30 @@ public class RemoveDuplicateLetters {
         int cnt[] = new int[26];
         for (int i = 0; i < s.length(); i++) 
             cnt[s.charAt(i) - 'a']++;
-        String out = new String();
+
+        boolean visited[] = new boolean[26];
         
-        return out;
+        Stack<Character> q = new Stack<Character>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int index = c - 'a';
+            cnt[index]--;
+            if (visited[index])
+                continue;
+            while (!q.isEmpty() && q.peek() > c && cnt[q.peek()-'a']>0) {
+                visited[q.pop()-'a'] = false;
+            }
+            q.push(c);
+            visited[index]=true;
+            
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!q.isEmpty()) {
+            sb.append(q.pop());
+        }
+
+        return sb.reverse().toString();
+
     }
 }
