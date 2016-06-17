10a116a8dfbc4f187ab237ea0df2e396d8dba23e
ghost2004 <liutg@hotmail.com>
2016-06-16 23:02:03 -0700

    new way

--- src/math/IntegerBreak.java
+++ src/math/IntegerBreak.java
@@ -30,4 +30,19 @@ public class IntegerBreak {
         }
         return dp[n];
     }
+	
+	public int intBreak(int n) {
+	    if (n <= 3)
+	        return n-1;
+
+	    int dp[] = new int[n+1];
+	    dp[1] = 1;
+	    dp[2] = 2;
+	    dp[3] = 3;
+	    
+	    for (int i=4; i <= n; i++)
+	        dp[i] = Math.max(dp[i-2]*2, dp[i-3]*3);
+	    
+	    return dp[n];
+	}
 }
