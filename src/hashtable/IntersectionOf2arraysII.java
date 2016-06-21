507094e325778d36b9d7e8e55252dc2c948764d3
ghost2004 <liutg@hotmail.com>
2016-06-21 12:24:03 -0700

    Leetcode 350. Intersection of Two Arrays II

---
+++ src/hashtable/IntersectionOf2arraysII.java
@@ -0,0 +1,47 @@
+package hashtable;
+/*
+ * Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+Note:
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+Follow up:
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you 
+cannot load all elements into the memory at once?
+ */
+import java.util.*;
+public class IntersectionOf2arraysII {
+	public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i:nums1) {
+        	Integer cnt = map.get(i);
+        	if (cnt == null) {
+        		map.put(i, 1);
+        	} else {
+        		map.put(i, cnt+1);
+        	}
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int y:nums2) {
+        	Integer cnt = map.get(y);
+        	if (cnt != null) {
+        		if (cnt > 0) {
+        			list.add(y);
+            		map.put(y, cnt-1);   			
+        		}
+        		
+        	}
+        }
+        int result[] = new int[list.size()];
+        int idx = 0;
+        for (int z:list) {
+        	result[idx++] = z;
+        }
+        return result;
+    }
+}
