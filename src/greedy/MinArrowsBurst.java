54baae5c8283545cbc2d3beb13e611e0d0d9126a
qa-mbp-b-2 <qa-mbp-b-2@192.168.0.62>
2017-10-07 11:16:28 -0700

    Leetcode 452. Minimum Number of Arrows to Burst Balloons

---
+++ src/greedy/MinArrowsBurst.java
@@ -0,0 +1,50 @@
+package greedy;
+
+import java.util.Arrays;
+
+/*
+ * Leetcode 452. Minimum Number of Arrows to Burst Balloons
+ * 
+ * There are a number of spherical balloons spread in two-dimensional space. 
+ * For each balloon, provided input is the start and end coordinates of the 
+ * horizontal diameter. Since it's horizontal, y-coordinates don't matter 
+ * and hence the x-coordinates of start and end of the diameter suffice. 
+ * Start is always smaller than end. There will be at most 104 balloons.
+
+    An arrow can be shot up exactly vertically from different points along the x-axis. 
+    A balloon with xstart and xend bursts by an arrow shot at x if xstart <=x <=xend. 
+    There is no limit to the number of arrows that can be shot. An arrow once shot 
+    keeps travelling up infinitely. The problem is to find the minimum number of 
+    arrows that must be shot to burst all balloons.
+    
+    Example:
+    
+    Input:
+    [[10,16], [2,8], [1,6], [7,12]]
+    
+    Output:
+    2
+    
+    Explanation:
+    One way is to shoot one arrow for example at x = 6 (bursting the balloons [2,8] 
+    and [1,6]) and another arrow at x = 11 (bursting the other two balloons).
+
+ */
+public class MinArrowsBurst {
+    public int findMinArrowShots(int[][] points) {
+        if (points == null || points.length == 0)
+            return 0;
+        int arrowCnt = 1;
+        Arrays.sort(points, (a, b) -> a[1]-b[1]);
+        int pos = points[0][1];
+        
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] <= pos)
+                continue;
+            arrowCnt++;
+            pos = points[i][1];
+        }
+        
+        return arrowCnt;
+    }
+}
