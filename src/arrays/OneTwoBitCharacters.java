1eb9394487f8cd7eaa7300b7493ab344b59c4204
ghost2004 <liutg@hotmail.com>
2017-10-28 19:58:32 -0700

    LeetCode 717. 1-bit and 2-bit Characters

---
+++ src/arrays/OneTwoBitCharacters.java
@@ -0,0 +1,40 @@
+package arrays;
+/*
+ * LeetCode 717. 1-bit and 2-bit Characters
+ * We have two special characters. The first character can be represented by one bit 0. The second character can 
+ * be represented by two bits (10 or 11).
+
+Now given a string represented by several bits. Return whether the last character must be a one-bit character or 
+not. The given string will always end with a zero.
+
+    Example 1:
+    Input: 
+    bits = [1, 0, 0]
+    Output: True
+    Explanation: 
+    The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
+    Example 2:
+    Input: 
+    bits = [1, 1, 1, 0]
+    Output: False
+    Explanation: 
+    The only way to decode it is two-bit character and two-bit character. So the last character is NOT one-bit character.
+    Note:
+    
+    1 <= len(bits) <= 1000.
+    bits[i] is always 0 or 1.
+ */
+public class OneTwoBitCharacters {
+    public boolean isOneBitCharacter(int[] bits) {
+        if (bits == null || bits.length < 2)
+            return true;
+        int idx = 0;
+        while (idx < bits.length-1) {
+            if (bits[idx] == 1)
+                idx += 2;
+            else
+                idx += 1;
+        }
+        return idx < bits.length;
+    }
+}
