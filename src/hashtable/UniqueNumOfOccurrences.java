a3bb878e8a952f791463658753a6b9e523f6211a
ghost2004 <liutg@hotmail.com>
2020-08-14 21:53:52 -0700

    Leetcode 1207. Unique Number of Occurrences

---
+++ src/hashtable/UniqueNumOfOccurrences.java
@@ -0,0 +1,47 @@
+package hashtable;
+/*
+ * Leetcode 1207. Unique Number of Occurrences
+ * Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+ */
+import java.util.*;
+public class UniqueNumOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        HashSet<Integer> set = new HashSet<>();
+        for (Integer i : map.values()) {
+            if (set.contains(i))
+                return false;
+            set.add(i);
+        }
+        
+        return true;
+        
+    }
+}
